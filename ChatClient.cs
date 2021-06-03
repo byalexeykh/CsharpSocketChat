fbea1c298712db2c5f43543dde33edb5235de254
byalexeykh <alek8901@gmail.com>
2021-06-03 19:53:35 +0200

    init

---
+++ ChatClient.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading;
+
+
+namespace SocketChat
+{
+    class ChatClient
+    {
+        static string NickName = "";
+        static TcpClient ClientSock = new TcpClient();
+        static NetworkStream ServerStream;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started chat client");
+            try
+            {
+                Console.WriteLine("Attempting to connect (127.0.0.1)...");
+                ClientSock.Connect(IPAddress.Parse("127.0.0.1"), 2222);
+                ServerStream = ClientSock.GetStream();
+            }catch(Exception e)
+            {
+                Console.WriteLine("\nConnection to 127.0.0.1 failed: " + e.ToString());
+                Console.WriteLine("\nPress any key to exit");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Connection established");
+            Console.WriteLine("Getting nickname...");
+
+            string msg;
+            do
+            {
+                int Size = ClientSock.ReceiveBufferSize;
+                byte[] bytes = new byte[1024];
+                ServerStream.Read(bytes, 0, bytes.Length);
+                msg = Encoding.ASCII.GetString(bytes);
+            } while (!InitNickname(ref msg, ref NickName));
+
+            Thread RefreshThread = new Thread(WaitForMessage);
+            RefreshThread.Start();
+            Console.WriteLine("Refresh thread started - searching for incoming messages in backgorund...");
+
+            while (true)
+            {
+                Console.Write(">> ");
+                string data = Console.ReadLine();
+                if (data == "" || data == null) break;
+                try
+                {
+                    byte[] bytes = new byte[1024];
+                    bytes = Encoding.ASCII.GetBytes(NickName + ": " + data);
+                    ClientSock.GetStream().Write(bytes, 0, bytes.Length);
+                }catch(Exception e)
+                {
+                    Console.Write("Failed to send message: " + e.ToString());
+                }
+            }
+        }
+
+        static void WaitForMessage()
+        {
+            while (true)
+            {
+                int Size = ClientSock.ReceiveBufferSize;
+                byte[] bytes = new byte[1024];
+                ServerStream.Read(bytes, 0, bytes.Length);
+                string msg = Encoding.ASCII.GetString(bytes);
+                Console.WriteLine("\n>> " + msg);
+            }
+        }
+
+        static bool InitNickname(ref string DataIn, ref string NickNameOut)
+        {
+            int length = DataIn.ToCharArray().Length;
+            if (DataIn.Contains("%"))
+            {
+                List<char> temp = new List<char>();
+                for (int i = DataIn.IndexOf("%") + 1; i < length; i++)
+                {
+                    temp.Add(DataIn[i]);
+                }
+                NickNameOut = new string(temp.ToArray());
+                Console.WriteLine("Your nickname: " + NickNameOut);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ ChatServer.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading;
+using System.Net.NetworkInformation;
+
+namespace Server
+{
+    class ChatServer
+    {
+        static Dictionary<string, TcpClient> Clients = new Dictionary<string, TcpClient>();
+        static TcpListener ServerSock = new TcpListener(IPAddress.Parse("127.0.0.1"), 2222);
+        static ReaderWriterLockSlim rwl = new ReaderWriterLockSlim();
+        static void Main(string[] args)
+        {
+            ServerSock.Start();
+            Console.WriteLine("Chat server stared");
+
+            Thread ConnectionThread = new Thread(HandleConnections);
+            ConnectionThread.Start();
+            
+            Thread RefreshThread = new Thread(HandleIncomingMessages);
+            RefreshThread.Start();
+        }
+
+        static private void Multicast(string ClientName, ref string message, ref List<TcpClient> Clients)
+        {
+            int i = 0;
+            foreach (TcpClient Client in Clients)
+            {
+                i++;
+                if (message != null && message != "")
+                {
+                    byte[] data = Encoding.ASCII.GetBytes(message);
+                    NetworkStream stream = Client.GetStream();
+                    stream.Write(data, 0, data.Length);
+                    stream.Flush();
+                }
+            }
+            Console.WriteLine("Multicasting to {0} clients",  i);
+        }
+
+        static void HandleIncomingMessages()
+        {
+            Console.WriteLine("Refresh thread started - searching for incoming messages in backgorund...");
+            List<TcpClient> CachedClients = new List<TcpClient>();
+            List<string> CachedClientNames = new List<string>();
+            while (true)
+            {
+                rwl.EnterReadLock();
+                try
+                {
+                    foreach (KeyValuePair<string, TcpClient> Client in Clients)
+                    {
+                        if (!CachedClients.Contains(Client.Value))
+                        {
+                            CachedClients.Add(Client.Value);
+                            CachedClientNames.Add(Client.Key);
+                            Console.WriteLine("\nCached new client: " + Client.Key);
+                        }
+                    }
+                }
+                finally
+                {
+                    rwl.ExitReadLock();
+                    int i = 0;
+                    foreach (TcpClient client in CachedClients)
+                    {
+                        byte[] bytes = new byte[1024];
+                        NetworkStream stream = client.GetStream();
+                        int bytesCount = 0;
+                        while (stream.DataAvailable)
+                        {
+                            bytesCount = stream.Read(bytes, 0, bytes.Length);
+                        }
+                        string data = System.Text.Encoding.ASCII.GetString(bytes, 0, bytesCount);
+                        if (data != null && data != "")
+                        {
+                            Multicast(CachedClientNames[i], ref data, ref CachedClients);
+
+                            Console.WriteLine("Received " + data + " from " + CachedClientNames[i] + ".");
+                            i++;
+                        }
+                    }
+                }
+            }
+        }
+
+        static void HandleConnections()
+        {
+            int ClientIndex = 0;
+            while (true)
+            {
+                if (ServerSock.Pending())
+                {
+                    byte[] bytes = new byte[1024];
+                    TcpClient ClientSock = ServerSock.AcceptTcpClient();
+                    Console.WriteLine("\nEstablishing new connection...");
+
+                    ClientIndex++;
+                    rwl.EnterWriteLock();
+                    try
+                    {
+                        Console.WriteLine("Locked Clients");
+                        Clients.Add("Member " + ClientIndex, ClientSock);
+                        Console.WriteLine("Connection establieshed with " + "Member " + ClientIndex);
+                    }
+                    finally
+                    {
+                        rwl.ExitWriteLock();
+                        Console.WriteLine("Lock released");
+                    }
+                    bytes = Encoding.ASCII.GetBytes("Your name in chat is: %" + "Member " + ClientIndex);
+                    ClientSock.GetStream().Write(bytes, 0, bytes.Length);
+                    ClientSock.GetStream().Flush();
+                }
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
